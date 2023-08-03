f4852e0e32cc954d4af5b468fdedcf95c8969589
prakashtimilsina <80721127+prakashtimilsina@users.noreply.github.com>
2023-08-02 23:34:28 -0500

    First commit

---
+++ app/page.js
@@ -0,0 +1,13 @@
+import ContactForm from "@/components/contactForm";
+
+
+export default function Home() {
+  return (
+    <div className="p-4 max-w-3xl mx-auto bg-slate-200 rounded">
+      <h1 className="text-3xl font-bold">Contact Us</h1>
+      <p>Please fill in the form below</p>
+
+      <ContactForm />
+    </div>
+  )
+}
---
+++ components/ContactForm.jsx
@@ -0,0 +1,71 @@
+'use client';
+import { useState } from "react";
+
+const ContactForm = () => {
+  const [fullname, setFullname] = useState("");
+  const [email, setEmail] = useState("");
+  const [message, setMessage] = useState("");
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    console.log("Fullname: ", fullname)
+    console.log("Email: ", email)
+    console.log("Message: ", message)
+  };
+
+  return (
+    <>
+      <form
+        onSubmit={handleSubmit}
+        className="py-4 mt-4 border-t flex flex-col gap-5"
+      >
+        <div>
+          <label htmlFor="fullname">Full Name</label>
+          <input
+            onChange={(e) => setFullname(e.target.value)}
+            value={fullname}
+            type="text"
+            name="fullname"
+            id="fullname"
+            placeholder="Prakash Sharma"
+          />
+        </div>
+        <div>
+          <label htmlFor="email">Email</label>
+          <input
+            onChange={(e) => setEmail(e.target.value)}
+            value={email}
+            type="email"
+            name="email"
+            id="email"
+            placeholder="prakashsharma@gmail.com"
+          />
+        </div>
+
+        <div>
+          <label htmlFor="message">Your Message</label>
+          <textarea
+            onChange={(e) => setMessage(e.target.value)}
+            value={message}
+            className="h-32"
+            id="message"
+            placeholder="Type your message.."
+          ></textarea>
+        </div>
+        <button
+          className="bg-green-600 py-3 text-white font-bold rounded-lg"
+          type="submit"
+        >
+          Send
+        </button>
+      </form>
+      <div>
+        <div className="bg-slate-100 flex flex-col">
+          <div className="text-red-600 px-5 py-2 rounded-lg">Error Message</div>
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default ContactForm;
